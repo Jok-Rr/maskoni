ffc36fd5b0b08fdec84dffacc613c5eeaec1bb48
Flr77 <florian.barraud01@gmail.com>
2022-06-23 14:03:04 +0200

    Update(modul): change text card

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -15,14 +15,14 @@ const Team = () => {
             <figure>
               <img src={robin} alt="" />
             </figure>
-            <p className='name neontech'>ROBIN</p>
+            <p className='name neontech'>TOM</p>
             <p className='role'>Full Stack Dev</p>
           </div>
           <div className='m_2col s_2col card'>
             <figure>
               <img src={robin} alt="" />
             </figure>
-            <p className='name neontech'>ROBIN</p>
+            <p className='name neontech'>FLORIAN</p>
             <p className='role'>Full Stack Dev</p>
           </div>
         </div>
@@ -32,28 +32,28 @@ const Team = () => {
               <img src={robin} alt="" />
             </figure>
             <p className='name neontech'>ROBIN</p>
-            <p className='role'>Full Stack Dev</p>
+            <p className='role'>Marketing Expert</p>
           </div>
           <div className='m_2col s_2col card'>
             <figure>
               <img src={robin} alt="" />
             </figure>
-            <p className='name neontech'>ROBIN</p>
-            <p className='role'>Full Stack Dev</p>
+            <p className='name neontech'>BAPTISTE</p>
+            <p className='role'>Content Creator</p>
           </div>
           <div className='m_2col s_2col card'>
             <figure>
               <img src={robin} alt="" />
             </figure>
-            <p className='name neontech'>ROBIN</p>
-            <p className='role'>Full Stack Dev</p>
+            <p className='name neontech'>MARC</p>
+            <p className='role'>Artist</p>
           </div>
           <div className='m_2col s_2col card'>
             <figure>
               <img src={robin} alt="" />
             </figure>
-            <p className='name neontech'>ROBIN</p>
-            <p className='role'>Full Stack Dev</p>
+            <p className='name neontech'>DAENYS</p>
+            <p className='role'>Head of Community</p>
           </div>
         </div>
       </div>
